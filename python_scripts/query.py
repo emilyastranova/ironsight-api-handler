3f5425ccb523a08d421c558497143a3c7f5f1092
tyleraharrison <tyleraharrison@gmail.com>
2022-03-19 05:00:27 -0500

    Config is working

--- python_scripts/query.py
+++ python_scripts/query.py
@@ -11,26 +11,28 @@ import json
 #   }
 # }'
 
-# Load in configuration file
-with open('../config.json') as config_file:
-    config = json.load(config_file)
-    sql_server = config['sql_server']
-    sql_user = config['sql_user']
-    sql_pass = config['sql_pass']
-    sql_db = config['sql_db']
-
-    elastic_url = config['elasticsearch_url']
-    elastic_token = config['elasticsearch_api_token']
-
 
 # Make a GET request to the API
 def get(queryJSON, index=""):
+    # Load in configuration file
+    with open('../config.json') as config_file:
+        config = json.load(config_file)
+        sql_server = config['sql_server']
+        sql_user = config['sql_user']
+        sql_pass = config['sql_pass']
+        sql_db = config['sql_db']
+
+        elastic_url = config['elasticsearch_url']
+        elastic_token = config['elasticsearch_api_token']
+
     # Swap port to 9200 for Kibana
     # TODO: Make this go through Nginx and have it divide traffic
-    elastic_url.replace(":8220", ":9200")
+    elastic_url = elastic_url.replace(":8220", ":9200")
     elastic_url = elastic_url + index + "/_search"
     headers = {"Content-Type" : "application/json", "Authorization": str("ApiKey " + elastic_token)}
     response = requests.get(elastic_url, headers=headers, data=queryJSON)
+    print(elastic_url)
+    print(response)
     return json.dumps(response.json())
 
 if __name__ == "__main__":
