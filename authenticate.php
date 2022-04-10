7568d76815094e511569cbc07a38ff3ebadcd924
tyleraharrison <tyleraharrison@gmail.com>
2022-04-10 18:20:51 -0500

    Better auth

--- authenticate.php
+++ authenticate.php
@@ -1,7 +1,11 @@
-<!-- Receive username and password from POST body -->
 <?php
-$username = $_POST["username"];
-$password = $_POST["password"];
+header('Content-type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+$data = json_decode(file_get_contents('php://input'), true);
+$username = $data["username"];
+$password = $data["password"];
 
 // Check against database
 $sql_server = "ssh.rellis.dev";
@@ -12,22 +16,31 @@ $dbname = "ironsight";
 // Create connection
 $conn = new mysqli($sql_server, $sql_username, $sql_password, $dbname);
 
+// Check connection
+if ($conn->connect_error) {
+    die('{"status": "no_connection"}');
+}
+
 // Table is users in ironsight database
-$sql = "SELECT password FROM users WHERE username = '$username'";
+$sql = "SELECT password FROM ironsight.users WHERE user_name = '$username'";
 $result = $conn->query($sql);
 
 // If user exists, check password
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        if ($row["password"] == $password) {
-            echo "true";
+        // if ($row["password"] == $password) {
+        //     echo '{"status": "success"}';
+        // }
+        // Check against SHA-512 hash, 4096 iterations
+        if (password_verify($password, $row["password"])) {
+            echo '{"status": "success"}';
         }
         else {
-            echo "false";
+            echo '{"status": "wrong_password"}';
         }
     }
 }
 else {
-    echo "false";
+    echo '{"status": "user_not_found"}';
 }
