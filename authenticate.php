4becf4d379f680ed01f5fab52891c8c5a700b9f9
tyleraharrison <tyleraharrison@gmail.com>
2022-04-10 19:29:30 -0500

    Attempt to fix CORS

--- authenticate.php
+++ authenticate.php
@@ -2,7 +2,7 @@
 header('Content-type: application/json');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
-header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+header('Access-Control-Allow-Headers: *');
 $data = json_decode(file_get_contents('php://input'), true);
 $username = $data["username"];
 $password = $data["password"];
