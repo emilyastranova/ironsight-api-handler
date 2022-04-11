d4486035ba0c7e5bd03656305db0200e615e9ffe
tyleraharrison <tyleraharrison@gmail.com>
2022-04-10 19:53:01 -0500

    CORS fix attempt

--- authenticate.php
+++ authenticate.php
@@ -1,8 +1,9 @@
 <?php
-// header('Content-type: application/json');
+header('Content-type: application/json');
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST');
-header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Methods: POST, OPTIONS');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
 $data = json_decode(file_get_contents('php://input'), true);
 $username = $data["username"];
 $password = $data["password"];
