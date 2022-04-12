8f46101cb685070219200f144117bee8082c104b
tyleraharrison <tyleraharrison@gmail.com>
2022-04-12 17:28:01 -0500

    Upgrade create_vm to support customization

--- create_vm.php
+++ create_vm.php
@@ -3,22 +3,22 @@ header('Content-type: application/json');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
 header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
-// $vm_name = $_GET["name"];
-// $vm_template = $_GET["template"];
-// $vm_username = $_GET["username"];
 
 // Take POST request with JSON data (template_name, user_name, vm_name, is_elastic)
 $data = json_decode(file_get_contents('php://input'), true);
 $vm_name = $data["vm_name"];
 $vm_template = $data["template_name"];
 $vm_username = $data["user_name"];
-$is_elastic = $data["is_elastic"];
+$template_override = $data["template_override"];
 
 // Make sure command is safe (no shell injection)
 $vm_name = escapeshellcmd($vm_name);
+$vm_template = escapeshellcmd($vm_template);
+$vm_username = escapeshellcmd($vm_username);
+$template_override = escapeshellcmd($template_override);
 
 chdir('/var/www/ironsight-api-handler/scripts/ironsight_harvester_api/');
-$command = 'python3 create_vm.py ' . $vm_name . ' ' . $vm_template . ' ' . $vm_username;
+$command = 'python3 create_vm.py ' . $vm_name . ' ' . $vm_template . ' ' . $vm_username . ' ' . $template_override;
 $output = shell_exec($command);
 // Get rid of newlines in output
 $output = str_replace("\n", ", ", $output);
